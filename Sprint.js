4b822b79d0f6e6413a1f670447806d8edd1beed4
SigridM <sigridm@selfhelpsoftware.com>
2019-12-02 12:37:01 -0500

    Remove getter for Sprint.backlogItems()

--- Sprint.js
+++ Sprint.js
@@ -26,29 +26,14 @@ class Sprint extends DatabaseTable {
         this.duration = aNumber;
     }
 
+    /* This may not get used, since the BLI's call #addBacklogItem() when 
+     * they loadSprints()
+     */
     loadBacklogItems(){
         this.backlogItems = SprintToBLIMapping.getBLIsFor(this.id);
     }
 
-    // Answer the backlogItems array.  Note that the backlogItems instance
-    // variable must be lazy-initialized to avoid getting into a mutual
-    // recursion when loading from the database.  (If instead we attempted
-    // to #loadBacklogItems() right afer setting the Sprint's ID, as we do
-    // when we loadSprints() in BacklogItem>>setID(), Sprint>>loadBacklogItems()
-    // would get called in the middle of BacklogItem>>loadSprints(), which would
-    // then try to instantiate a Sprint and come right back to 
-    // Sprint>>loadBacklogItems().)
-    backlogItems() {
-        if (this.backlogItems === null) {
-            this.loadBacklogItems();
-        }
-        return this.backlogItems;
-    }
-
     addBacklogItem(aBacklogItem) {
-        // if (this.backlogItems === null) {
-        //     this.backlogItems = [];
-        // }
         this.backlogItems.push(aBacklogItem);
     }
 }
