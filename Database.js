7aa59f7a32baa11bbfa3717b0270a538c60504a5
SigridM <sigridm@selfhelpsoftware.com>
2019-12-04 10:07:52 -0500

    Comment Database class and methods

--- Database.js
+++ Database.js
@@ -1,7 +1,24 @@
+/**
+ * The Database class is, for now, a proxy for a real database interface.  It
+ * retrieves all the records for any subclass of DatabaseTable, then instantiates
+ * each of those records into an instance of that subclass.  Finally, it stores
+ * the instances in a Map, keyed by the value of each instance's value of its
+ * keyColumn.  That map will be stored in a static variable of the class.
+ * 
+ * @todo Eventually, Database will perform the same actions using an SQL interface.
+ */
 class Database {
+
+    /**
+     * Retrieve from the database all instances of the receiver class.
+     * @param {DatabaseClass} aClass The subclass of DatabaseTable that
+     * will be instantiated for each of its records in the database.
+     * @returns a Map of all the instances, keyed by each instance's value
+     * of the class' keyColumn.
+     */
     static loadAllForClass(aClass) {
         var map = new Map();
-        var records = this.allRecordsFrom(aClass.tableName);
+        var records = Record.recordsFor(aClass.tableName);
         for (var eachRecord of records) {
             var instance = this.instantiateFrom(aClass, eachRecord);
             var keyColumn = aClass.keyColumn;
@@ -10,42 +27,69 @@ class Database {
         return map;
     }
 
+    /**
+     * Retrieve from the database all instances of the receiver class 
+     * where the value in the column named aColumnName matches aValue.
+     * @param {DatbaseClass} aClass The subclasss of DatabaseTable that 
+     * will be instantiated for each of the records in the database 
+     * satisfying the criteria.
+     * @param {String} aColumnName The name of the column in which to
+     * search for the value.
+     * @param {Object} aValue The value for which to search.
+     * @returns a Map of all the instances matching the criteria, keyed 
+     * by each instance's value of the class' keyColumn.
+     * @todo if loaded, should we perhaps also search the records already
+     * loaded instead of hitting the database for all records?  The issue
+     * is that instantiating a new instance from the database will answer
+     * a different object than those already instantiated.
+     */
     static loadAllForClassWhere(aClass, aColumnName, aValue) {
-        // This will be done with a SQL Where statement ultimately.
         var map = new Map();
-        var records = this.allRecordsWhere(aClass.tableName, aColumnName, aValue);
+        var records = Record.recordsWhere(aClass.tableName, aColumnName, aValue);
         for (var eachRecord of records) {
             var instance = this.instantiateFrom(aClass, eachRecord);
             var keyColumn = aClass.keyColumn;
             map.set(eachRecord.at(keyColumn), instance);
         }
         return map;
-
     }
 
-    static allRecordsFrom(tableName) {
-        var answer = Record.recordsFor(tableName);
-        return answer;
-    }
-    static allRecordsWhere(tableName, aColumnName, aValue) {
-        // This will be done with a SQL WHERE clause eventually
-        var answer = Record.recordsWhere(tableName, aColumnName, aValue);
-        return answer;
-    }
-
+    /**
+     * Create an instance of aClass from the data in aRecord.
+     * @param {DatbaseClass} aClass The subclass of DatabaseClass to
+     * instantiate.
+     * @param {Record} aRecord The database record containing the data
+     * for the instance.
+     * @returns an instance of aClass
+     */
     static instantiateFrom(aClass, aRecord) {
         var instance = new aClass();
         var columns = aClass.columns;
         for (var eachColumnName of columns) {
             var value = aRecord.at(eachColumnName);
-            var setterFunctionName = "set" + eachColumnName;
-            var setterFunction = aClass.prototype[setterFunctionName];
-            if (setterFunction == undefined) {
-                console.log("For class: " + aClass.name, " Undefined setter function: " + setterFunctionName);
-            } else {
-                setterFunction.call(instance, value);
-            }
+            var setterFunction = this.setterFunctionFor(eachColumnName, aClass);
+            setterFunction.call(instance, value);
         }
         return instance;
+    }
+
+    /**
+     * Find and return the setter function in aClas that matches aColumnName.
+     * @param {String} aColumnName // The name of the column in the database
+     * that will be set in the instance using the setterFunction.
+     * @param {DatabaseClass} aClass // The subclass of DatabaseClase that
+     * is having one of its properties set using the setterFunction
+     * @returns the function of aClass that sets thre property aColumnName.
+     * @todo Invoke a real error if the function is not found, rather than 
+     * output to the console.
+     */
+    static setterFunctionFor(aColumnName, aClass) {
+        var setterFunctionName = "set" + aColumnName;
+        var setterFunction = aClass.prototype[setterFunctionName];
+        if (setterFunction == undefined) {
+            console.log("For class: " + aClass.name, 
+            " Undefined setter function: " + setterFunctionName);
+        }
+        return setterFunction;
     }
 }
