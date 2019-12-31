1cc25f47769eada8d8a70602f93d83e2c82c1af6
SigridM <sigridm@selfhelpsoftware.com>
2019-12-30 21:42:30 -0500

    Add DateRange methods

--- DateRange.js
+++ DateRange.js
@@ -34,4 +34,57 @@ class DateRange {
                 (aDate <= endDate)
     }
 
+    /**
+     * Answer whether the date range completely encloses another
+     * date range.  Any of following will return true:
+     *   receiver:  |start|-----------------------|end|
+     *   parameter:       |start|-----------|end|
+     * 
+     *   receiver:  |start|-----------------------|end|
+     *   parameter: |start|-----------|end|
+     * 
+     *   receiver:  |start|-----------------------|end|
+     *   parameter:             |start|-----------|end|
+     * 
+     *   receiver:  |start|-----------------------|end|
+     *   parameter: |start|-----------------------|end|
+     * 
+     * @param {DateRange} aDateRange
+     * @returns {Boolean} whether the parameter is completely within
+     * the receiver
+     */
+    encompasses(aDateRange) {
+        return (this.startDate <= aDateRange.startDate) &&
+        (aDateRange.endDate <= this.endDate);
+    }
+
+    /**
+     * Answer whether there is any overlap of dates between
+     * the receiver and the parameter date ranges.
+     * In addition to the edge cases (see DateRange.encompasses()),
+     * Any of the following will return true:
+     *   receiver:  |start|-----------------------|end|
+     *   parameter:       |start|-----------|end|
+     * 
+     *   receiver:         |start|-----------|end|
+     *   parameter: |start|-----------------------|end|
+     * 
+     *   receiver:  |start|----------------|end|
+     *   parameter:        |start|----------------|end|
+     * 
+     *   receiver:         |start|----------------|end|
+     *   parameter: |start|----------------|end|
+     * 
+     * 
+     * @param {DateRange} aDateRange 
+     * @returns {Boolean} whether any of the dates within
+     * the receiver overlap with any of the dates within the parameter
+     */
+    overlaps(aDateRange) {
+        return (this.encompasses(aDateRange)) ||
+            (aDateRange.encompasses(this)) ||
+            (this.startDate <= aDateRange.startDate && this.endDate <= aDateRange.endDate) ||
+            (aDateRange.startDate <= this.startDate && aDateRange.endDate <= this.endDate)
+
+    }
 }
